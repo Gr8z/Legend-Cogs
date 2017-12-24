8e6ae3c28a0c35cf5953fbf21fa0f684606974b1
GR8 <gr8_boi52@yahoo.com>
2017-12-24 16:23:00 +0400

    added desc in !waitinglist

--- legend/legend.py
+++ legend/legend.py
@@ -934,7 +934,9 @@ class legend:
 
         await self.bot.type()
 
-        embed=discord.Embed(title="", description="", color=0x0080ff)
+        
+        totalMembers = sum(clans[x]['memberCount'] for x in range(len(clans)))
+        embed=discord.Embed(title="", description="Our Family is made up of " + str(self.numClans()) + " clans with a total of " + str(totalMembers) + " members. We have " + str((self.numClans()*50)-totalMembers) + " spots left.", color=0x0080ff)
 
         for indexC, clan in enumerate(self.c):
             if self.c[clan]["waiting"]:
