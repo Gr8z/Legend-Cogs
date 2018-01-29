ca535b183e5e2ba51f1ff1555056a57dd0d86dd3
Bobloy <alboblexloy@gmail.com>
2018-01-29 10:39:37 -0500

    self.

--- tourney/tourney.py
+++ tourney/tourney.py
@@ -115,7 +115,7 @@ class tournament:
 	async def getTopTourneyNew(self):
 
 		try:
-			tourneydata = await _fetchTourney()
+			tourneydata = await self._fetchTourney()
 		except (requests.exceptions.Timeout, json.decoder.JSONDecodeError):
 			return None
 		except requests.exceptions.RequestException as e:
@@ -201,7 +201,7 @@ class tournament:
 		    return
 
 		try:
-			tourneydata = await _fetchTourney()
+			tourneydata = await self._fetchTourney()
 		except (requests.exceptions.Timeout, json.decoder.JSONDecodeError):
 			await self.bot.say("Error: cannot reach Clash Royale Servers. Please try again later.")
 			return
