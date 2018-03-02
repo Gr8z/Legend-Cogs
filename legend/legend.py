1562213cd75663ec01d732d5b5e6ca24f4b79194
milan_25 <35848213+milan252525@users.noreply.github.com>
2018-03-02 14:49:12 +0100

    Legend clan on 11th position wouldn't show

--- legend/legend.py
+++ legend/legend.py
@@ -1271,7 +1271,7 @@ class legend:
         
             for x in range(10):
                 msg += ((str(topclans[x]["rank"]) + ".").ljust(4) + topclans[x]["name"] + "\n")
-            for i in range(11, len(topclans)):
+            for i in range(10, len(topclans)):
                 for j in self.c:
                     if topclans[i]["tag"] == self.c[j]["tag"]:
                         msg += ((str(topclans[i]["rank"]) + ".").ljust(4) + topclans[i]["name"] + "\n")    
