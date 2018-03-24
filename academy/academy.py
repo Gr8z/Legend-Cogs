86995b842710d5bd071c3162ebf98f48a46c93a4
milan_25 <milan.abraham2@seznam.cz>
2018-03-24 14:05:34 +0100

    Added !coaching

--- academy/academy.py
+++ academy/academy.py
@@ -81,6 +81,96 @@ class academy:
             to_delete.append(message)
             
         await self.mass_purge(to_delete)   
+        
+    @commands.command(pass_context=True)
+    async def coaching(self, ctx):  
+        msg_req = "**Request by:** {}\n**In-game name:** {}\n**Trophies:** {}\n**Wants to learn:** {}\n**Time:** {}\n**Additional info:** {}\n{}"
+        msg_timeout = "I had to wait too long. Start over by typing !coaching again!"
+        msg_stop = "Stopped."
+        msg_err = "Process was interrupted! Please do not send messages in LeGeND server while filling the request! Start over by typing !coaching."
+        msg_start = "I will ask you few questions, let's move to DM!"
+        msg_start_dm = "Please do not send any message to LeGeND server while filling this request! You can stop anytime by typing \"stop\". Let's start!"
+        
+        user = ctx.message.author
+        channel_req = self.bot.get_channel("426689763173597185")
+        server_req = self.bot.get_server("374596069989810176")
+        coach_role= discord.utils.get(server_req.roles, name="Coach")
+    
+        await self.bot.send_message(ctx.message.channel, msg_start)
+        await self.bot.send_message(user, msg_start_dm)
+        await asyncio.sleep(3)
+        await self.bot.send_message(user, "What's your in-game name?")
+        reply = (await self.bot.wait_for_message(author=user, timeout=90))
+        if reply == None:
+            await self.bot.send_message(user, msg_timeout)
+            return
+        elif reply.content.lower() == "stop":
+            await self.bot.send_message(user, msg_stop)
+            return
+        if not reply.channel.is_private:
+            await self.bot.send_message(user, msg_err)
+            return
+        else:
+            ingame_name = reply.content
+                 
+        await self.bot.send_message(user, "How many trophies do you have?")
+        reply = (await self.bot.wait_for_message(author = user, timeout=90))
+        if reply == None:
+            await self.bot.send_message(user, msg_timeout)
+            return
+        elif reply.content.lower() == "stop":
+            await self.bot.send_message(user, msg_stop)
+            return
+        if not reply.channel.is_private:
+            await self.bot.send_message(user, msg_err)
+            return
+        else:
+            trophies = reply.content
+        
+        await self.bot.send_message(user, "What archetype/deck do you want to learn?")
+        reply = (await self.bot.wait_for_message(author=user, timeout=120))
+        if reply == None:
+            await self.bot.send_message(user, msg_timeout)
+            return
+        elif reply.content.lower() == "stop":
+            await self.bot.send_message(user, msg_stop)
+            return
+        if not reply.channel.is_private:
+            await self.bot.send_message(user, msg_err)
+            return
+        else:
+            info = reply.content 
+        
+        await self.bot.send_message(user, "What time do you prefer (with timezones) for coaching?")
+        reply = (await self.bot.wait_for_message(author=user, timeout=120))
+        if reply == None:
+            await self.bot.send_message(user, msg_timeout)
+            return
+        elif reply.content.lower() == "stop":
+            await self.bot.send_message(user, msg_stop)
+            return
+        if not reply.channel.is_private:
+            await self.bot.send_message(user, msg_err)
+            return
+        else:
+            time = reply.content        
+        
+        await self.bot.send_message(user, "Do you have anything else you want to add? (Type \"No\" if not)")
+        reply = (await self.bot.wait_for_message(author = user, timeout=120))
+        if reply == None:
+            await self.bot.send_message(user, msg_timeout)
+            return
+        elif reply.content.lower() == "stop":
+            await self.bot.send_message(user, msg_stop)
+            return
+        if not reply.channel.is_private:
+            await self.bot.send_message(user, msg_err)
+            return
+        else:
+            more_info = reply.content        
+        
+        await self.bot.send_message(channel_req, msg_req.format(user, ingame_name, trophies, info, time, more_info, coach_role.mention))
+        await self.bot.send_message(user, "Ok! I sent your request to coaches, someone will get to you as soon as possible.")
 
 
 def setup(bot):
