a541e076fa65e34393e2bf9c787f3a10e4105bce
GR8 <gr8_boi52@yahoo.com>
2018-05-29 15:20:19 +0400

    fix spam

--- fmod/fmod.py
+++ fmod/fmod.py
@@ -994,8 +994,7 @@ class fmod:
             except:
                 continue
         await self.bot.say("This warning was not found. Please make sure you typed it correctly!")
-      
-                    
+                     
     @commands.command(no_pm=True, pass_context=True)
     @checks.admin()
     async def delwarn(self, ctx, id, *, reason):
@@ -1202,9 +1201,9 @@ class fmod:
                         dataIO.save_json(self.warnings, self.warningsload)
                         return
                     # else:
-                await self.bot.say("This warning was not found. Please make sure you typed it correctly!")
             except:
                 continue
+        await self.bot.say("This warning was not found. Please make sure you typed it correctly!")
                 
     @commands.command(no_pm=True, pass_context=True)
     async def report(self, ctx, user: discord.Member):
