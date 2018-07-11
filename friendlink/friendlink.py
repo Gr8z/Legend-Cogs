7812ad4a93a827f6ed8a943e7ca7b2a68bce487b
Gr8z <gr8_boi52@yahoo.com>
2018-07-11 14:48:23 +0500

    Added emojis to friendlinks

--- friendlink/friendlink.py
+++ friendlink/friendlink.py
@@ -19,6 +19,13 @@ class friendlink:
         self.auth = self.bot.get_cog('crtools').auth
         self.clash = clashroyale.Client(self.auth.getToken(), is_async=True)
 
+    def emoji(self, name):
+        """Emoji by name."""
+        for emoji in self.bot.get_all_emojis():
+            if emoji.name == name:
+                return '<:{}:{}>'.format(emoji.name, emoji.id)
+        return ''
+
     async def friend_link(self, message):
 
         url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.content)
@@ -40,14 +47,16 @@ class friendlink:
             else:
                 clanurl = profiledata.clan.badge.image
 
+            arenaFormat = profiledata.arena.arena.replace(' ', '').lower()
+
             embed=discord.Embed(title='Click this link to add as friend in Clash Royale!', url=url[0], color=0x0080ff)
             embed.set_author(name=profiledata.name + " (#"+profiledata.tag+")", icon_url=clanurl)
             embed.set_thumbnail(url="https://imgur.com/C9rLoeh.jpg")
             embed.add_field(name="User", value=message.author.mention, inline=True)
-            embed.add_field(name="Trophies", value=profiledata.trophies, inline=True)
+            embed.add_field(name="Trophies", value="{} {:,}".format(self.emoji(arenaFormat), profiledata.trophies), inline=True)
+            embed.add_field(name="Level", value="{} {}".format(self.emoji("level"), profiledata.stats.level), inline=True)
             if profiledata.clan is not None:
-                embed.add_field(name="Clan", value=profiledata.clan.name, inline=True)
-            embed.add_field(name="Arena", value=profiledata.arena.name, inline=True)
+                embed.add_field(name="Clan {}".format(profiledata.clan.role.capitalize()), value="{} {}".format(self.emoji("clan"), profiledata.clan.name), inline=True)
             embed.set_footer(text=credits, icon_url=creditIcon)
 
             await self.bot.send_message(message.channel, embed=embed)
@@ -55,6 +64,7 @@ class friendlink:
             await self.bot.delete_message(message)
 
         except Exception as e:
+            raise
             return
 
     async def on_message(self, message):
