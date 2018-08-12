2a35ca37c4fd9f2bcc5c0aafa28bf35662e899f6
Gr8z <gr8_boi52@yahoo.com>
2018-08-12 13:59:33 +0500

    Thanks @4JR

--- reactrole/reactrole.py
+++ reactrole/reactrole.py
@@ -6,6 +6,7 @@ from .utils.dataIO import dataIO, fileIO
 import os
 from cogs.utils import checks
 from copy import deepcopy
+import json
 
 settings_path = "data/reactrole/settings.json"
 default_settings = {"roles": {}, "messages": []}
@@ -25,6 +26,14 @@ class reactrole:
         if server.id not in self.settings:
             self.settings[server.id] = deepcopy(default_settings)
             dataIO.save_json(settings_path, self.settings)
+
+    async def formatEmoji(self, emoji):
+        if emoji["id"] is None:
+            return emoji["name"]
+        elif emoji["animated"]:
+            return'a:{}:{}'.format(emoji["name"], emoji["id"])
+        else:
+            return':{}:{}'.format(emoji["name"], emoji["id"])
 
     @commands.group(pass_context=True, name="reactrole")
     @checks.mod_or_permissions(administrator=True)
@@ -108,39 +117,38 @@ class reactrole:
         await self.bot.say("Reactrole will now check for reactions on ```{}```".format(msg.content))
         dataIO.save_json(settings_path, self.settings)
 
-    async def on_reaction_add(self, reaction, user):
-        if not reaction.message.channel.is_private and self.bot.user.id != user.id:
-            server = reaction.message.server
+    async def toggle_roles_reaction(self, data_type, server_id, message_id, user_id, emoji_data):
+        if self.bot.user.id != user_id:
+            server = self.bot.get_server(server_id)
+            user = server.get_member(user_id)
             if server.id in self.settings:
-                channel = reaction.message.channel
-                reactionEmoji = str(reaction.emoji).replace(">", "").replace("<", "")
-                if int(reaction.message.id) in self.settings[server.id]["messages"]:
+                reactionEmoji = await self.formatEmoji(emoji_data)
+                if int(message_id) in self.settings[server.id]["messages"]:
                     emoji = self.settings[server.id]["roles"]
                     if reactionEmoji in emoji.keys():
                         role = discord.utils.get(server.roles, id=emoji[reactionEmoji])
                         if role is None:
-                            return await self.bot.send_message(channel, "ReactRole: Role not found.")
+                            print("ReactRole: Role not found.")
                         try:
-                            await self.bot.add_roles(user, role)
+                            if data_type == "MESSAGE_REACTION_ADD":
+                                await self.bot.add_roles(user, role)
+                            elif data_type == "MESSAGE_REACTION_REMOVE":
+                                await self.bot.remove_roles(user, role)
                         except discord.errors.Forbidden:
-                            await self.bot.send_message(channel, "ReactRole: No permission to add roles.")
+                            print("ReactRole: No permission to add roles.")
 
-    async def on_reaction_remove(self, reaction, user):
-        if not reaction.message.channel.is_private and self.bot.user.id != user.id:
-            server = reaction.message.server
-            if server.id in self.settings:
-                channel = reaction.message.channel
-                reactionEmoji = str(reaction.emoji).replace(">", "").replace("<", "")
-                if int(reaction.message.id) in self.settings[server.id]["messages"]:
-                    emoji = self.settings[server.id]["roles"]
-                    if reactionEmoji in emoji.keys():
-                        role = discord.utils.get(server.roles, id=emoji[reactionEmoji])
-                        if role is None:
-                            return await self.bot.send_message(channel, "ReactRole: Role not found.")
-                        try:
-                            await self.bot.remove_roles(user, role)
-                        except discord.errors.Forbidden:
-                            await self.bot.send_message(channel, "ReactRole: No permission to remove roles.")
+    async def on_socket_raw_receive(self, raw_msg):
+        """ Listens to reaction adds/removes """
+        msg = json.loads(raw_msg)
+        data_type = msg['t']
+        msg_data = msg['d']
+
+        if data_type in ['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE']:
+            await self.toggle_roles_reaction(data_type,
+                                             msg_data['guild_id'],
+                                             msg_data['message_id'],
+                                             msg_data['user_id'],
+                                             msg_data['emoji'])
 
 
 def check_folders():
