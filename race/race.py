c013535a7c5f0e31082d97e255a8cfa0de022f51
Gr8z <gr8_boi52@yahoo.com>
2018-09-15 00:24:16 +0400

    less dash

--- race/race.py
+++ race/race.py
@@ -385,7 +385,7 @@ class Race:
         embed.add_field(name=first, value=fv)
         embed.add_field(name=second, value=sv)
         embed.add_field(name=third, value=tv)
-        embed.add_field(name='-' * 99, value='Type ``!race claim`` to receive prize money. \nType ``!togglerole race`` to get notified on the next race.')
+        embed.add_field(name='-' * 70, value='Type ``!race claim`` to receive prize money. \nType ``!togglerole race`` to get notified on the next race.')
         embed.title = "Race Results"
         embed.set_footer(text=credits, icon_url=creditIcon)
         await self.bot.say(embed=embed)
